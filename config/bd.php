dcc61f8b53dfb038a189fa5abc121700ba50138c
Raúl Fernández <raulfernandez484@gmail.com>
2023-08-12 11:44:20 +0200

    update port in bd.php

--- config/bd.php
+++ config/bd.php
@@ -7,7 +7,7 @@ class BD{
 
         if(!isset(self::$instancia)){
             $opciones[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-            self::$instancia = new PDO('mysql:host=localhost;port=3310;dbname=aplicacion', 'root', '', $opciones);
+            self::$instancia = new PDO('mysql:host=localhost;port=3306;dbname=aplicacion', 'root', '', $opciones);
             //echo "Conexión a la BBDD establecida";
         }
         return self::$instancia;
